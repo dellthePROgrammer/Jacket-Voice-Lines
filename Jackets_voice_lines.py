e0f0254378ddb5b1a908417ab81b755340213de7
dellthePROgrammer <delpierobatou@gmail.com>
2022-11-15 16:57:31 -0500

    sets python the runable folder

--- Jackets_voice_lines.py
+++ Jackets_voice_lines.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 # This file was made with the intention of being used with a halloween costume of the character Jacket from Payday 2 which allows the person to 
 # play voice lines of the character by pressing a button on their hand to play the corresponding voice lines
 # the idea of the random module to randomize the files as well as putting the voice_lines into a list was made by https://github.com/Kostowniak
